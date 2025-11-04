ec3f05479550de3be174e06faf101321b02e8bbd
davidgimeno <233226980+davidgimeno25@users.noreply.github.com>
2025-11-04 20:28:04 +0100

    cambios menores en buscar_por_id.py

--- ENTREGAS PYTHON/buscar_por_id.py
+++ ENTREGAS PYTHON/buscar_por_id.py
@@ -1,7 +1,7 @@
 def buscar_por_id(equipos):
     IDusuario = input("Escribe un ID: ")
     if IDusuario == equipos["id"]:
-        print(f"ID: {equipos["ID"]}")
+        print(f"ID: {equipos["id"]}")
         print(f"Nombre: {equipos["nombre"]}")
         print(f"Ciudad: {equipos["ciudad"]}")
         print(f"Estado: {"Activo" if equipos("estado") else "Inactivo"}")
